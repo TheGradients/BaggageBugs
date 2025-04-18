f6c0d6f36cb2da2846c3184a3b8244c5425996f2
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-04-18 22:12:54 +0530

    push to main

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,11 @@
 
 import React from "react";
 import { useState } from "react";
-import Body from "./components/Body";
+// import Body from "./components/Body";
 import Body from "./components/Body";
 import LandingPage from "./components/landingPage/LandingPage";
 import DashboardDetails from "./components/DashboardDetails/DashboardDetails";
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
 import Register from "./components/Register/register";
 import Partneroverview from "./components/Partner/Partneroverview";
 import Profile from "./components/Profile/Profile";
