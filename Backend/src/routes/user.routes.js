c77a82ae7abbda1bbf9d10625be4821215c624a4
anmol420 <anmolanand2k15@gmail.com>
2025-06-15 00:33:04 +0530

    Added ForgotPassword and Nodemailer

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -9,7 +9,10 @@ import {
     getUser,
     toggleEmail,
     googleCallback,
-    setCookies
+    setCookies,
+    forgotPasswordEmail,
+    forgotPasswordVerifyOTP,
+    forgotPassword
 } from '../controllers/user.controller.js';
 import verifyJWT from "../middlewares/auth.middleware.js";
 import googleAuth from "../middlewares/googleAuth.middleware.js"
@@ -18,7 +21,7 @@ import passport from "../libs/passport.libs.js";
 const router = Router();
 
 router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/login').post(login); 
 router.route('/auth/google').get(passport.authenticate('google', {
     scope: ['profile', 'email'],
     prompt: 'select_account',
@@ -27,6 +30,9 @@ router.route('/auth/google/callback').get(passport.authenticate('google', {
     failureRedirect: `${process.env.CLIENT_URL}`,
 }), googleAuth, googleCallback);
 router.route('/setCookies').post(setCookies);
+router.route('/forgotPasswordEmail').post(forgotPasswordEmail);
+router.route('/forgotPasswordVerifyOTP').post(forgotPasswordVerifyOTP);
+router.route('/forgotPassword').post(forgotPassword);
 
 router.route('/logout').post(verifyJWT, logout);
 router.route('/addDetails').post(verifyJWT, addDetails);
